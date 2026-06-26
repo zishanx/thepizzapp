b2424901d68c783d4a94644eb0c8501ffc182c38
zishanx <zishanalikhan53@gmail.com>
2026-06-26 22:49:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const menu = [
+    { name: "Margherita", price: 8},
+    {name: "Pepperoni", price : 10},
+    {name: "Hawaiian", price: 10},
+    {name: "Veggie", price:9},
+]
+
+const cashInRegister = 100
+
+const orderQueue = []
+
+function addNewPizza (){
+    
+}
