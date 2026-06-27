e43cd2b858b3837293261334668d22581c1bf237
zishanx <zishanalikhan53@gmail.com>
2026-06-27 10:55:45 +0530

    Switched to typeScript

--- index.ts
+++ index.ts
@@ -1,46 +1,59 @@
+type Dish = {
+    name: string
+    price: number
+}
 
-
-const menu = [
+const menu: Dish[] = [
     { name: "Margherita", price: 8 },
     { name: "Pepperoni", price: 10 },
     { name: "Hawaiian", price: 10 },
     { name: "Veggie", price: 9 },
 ]
 
-let cashInRegister = 100
-let nextOrderid = 1
-const orderQueue = []
+let cashInRegister: number = 100
+let nextOrderid: number = 1
+
+type Queue = {
+    pizza: Dish
+    status: "ordered" | "completed"
+    id: number
+}
+const orderQueue: Queue[] = []
 
 
 
-function addNewPizza(pizzaObj) {
+function addNewPizza(pizzaObj: Dish): void {
     menu.push(pizzaObj)
 }
 
-function placeOrder(pizzaname) {
+function placeOrder(pizzaname: string): Queue | void {
     const pizza = menu.find((item) => item.name === pizzaname);
-    if(!pizza){
+    if (!pizza) {
         console.log("The pizza doesn't exist in your menu.")
         return
     }
-    cashInRegister +=  pizza.price;
-    let newOrder = { pizza: pizza, status: "ordered", id: nextOrderid++ };
+    cashInRegister += pizza.price;
+    let newOrder: Queue = { pizza: pizza, status: "ordered", id: nextOrderid++ };
 
     orderQueue.push(newOrder)
 
     return newOrder
 }
 
-function completeOrder(id) {
+function completeOrder(id: number) {
     let findOrder = orderQueue.find(item => item.id === id);
+    if (!findOrder) {
+        console.log("Order not found")
+        return
+    }
     findOrder.status = "completed"
 
     return findOrder
 }
 
-addNewPizza({name: "Chicken Bacon Ranch", price: 12})
-addNewPizza({name: "BBQ Chicken", price: 10})
-addNewPizza({name: "Spicy Sausage", price: 11})
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 })
+addNewPizza({ name: "BBQ Chicken", price: 10 })
+addNewPizza({ name: "Spicy Sausage", price: 11 })
 
 placeOrder("Chicken Bacon Ranch")
 completeOrder(1)
--- learning.ts
+++ learning.ts
@@ -1,2 +1,17 @@
-let myName = "Zanth"
+interface Person  {
+    name: string
+    age: number 
+    isStudent: boolean
+}
 
+let person1: Person = {
+    name: "Joe",
+    age: 42,
+    isStudent: true
+}
+
+let person2: Person  = {
+    name: "Jill",
+    age: 66,
+    isStudent: false
+}
