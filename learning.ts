edc404a36e6c61c3da76f02f1b51003381228c2a
zishanx <zishanalikhan53@gmail.com>
2026-06-27 12:55:31 +0530

    working with optional key in Obj

--- learning.ts
+++ learning.ts
@@ -2,16 +2,31 @@ interface Person  {
     name: string
     age: number 
     isStudent: boolean
+    address:{
+        street:string
+        city:string
+        country:string
+    }
 }
 
 let person1: Person = {
     name: "Joe",
     age: 42,
-    isStudent: true
+    isStudent: true,
+    address:{
+        street:"Middhi",
+        city:"Ballia",
+        country:"India"
+    }
 }
 
 let person2: Person  = {
     name: "Jill",
     age: 66,
-    isStudent: false
+    isStudent: false,
+    address:{
+        street:"123 Main",
+        city: "Anyton",
+        country:"USA"
+    }
 }
