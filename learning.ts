1baaf468c87c45ec9cffe5b9ac81bc3b667b19a3
zishanx <zishanalikhan53@gmail.com>
2026-06-29 21:06:24 +0530

    learning literal type

--- learning.ts
+++ learning.ts
@@ -1,21 +1,3 @@
-type Person = {
-    name: string
-    age: number
-    isStudent: boolean
-}
+let myName = "Bob" 
 
-let person1: Person = {
-    name: "Joe",
-    age: 42,
-    isStudent: true
-}
-
-let person2: Person = {
-    name: "Jill",
-    age: 66,
-    isStudent: false
-}
-
-let people: Person[] = [person1, person2]
-
-//The stuff that i already know about includes type and interface union and intersector using & and also about eh a lil bit about the genirc type .
+const myName2 = "Bob"
